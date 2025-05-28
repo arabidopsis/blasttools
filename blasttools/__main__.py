ff16ca66f590a9f5349dd68b93f7876cd7b24e37
Ian Castleden <ian.castleden@uwa.edu.au>
2025-05-28 19:58:17 +0800

    ruff check

--- blasttools/__main__.py
+++ blasttools/__main__.py
@@ -1,8 +1,14 @@
 from __future__ import annotations
 
-from . import exact  # noqa: pylint: disable=unused-import
-from . import plants_ui  # noqa: pylint: disable=unused-import
+from . import exact
+from . import plants_ui
 from .cli import blast
+
+__all__ = [
+    "plants_ui",
+    "exact",
+    "blast",
+]
 
 if __name__ == "__main__":
     blast.main(prog_name="blasttools")  # pylint: disable=no-value-for-parameter
